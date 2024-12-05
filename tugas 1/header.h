90653fc61f9b558fd47a226ce7f45195a35fe5d2
hanihuri <huriyah24@mhs.usk.ac.id>
2024-12-05 20:29:47 +0700

    Update header.h
    File ini berisi implementasi header dari fungsi yang akan digunakan dalam Sistem Peminjaman Alat Lab

--- tugas 1/header.h
+++ tugas 1/header.h
@@ -26,3 +26,23 @@ typedef struct {
     unsigned int Jumlah_Pinjam;
 } Peminjaman;
 
+// Deklarasi variabel global
+extern Alat_Lab alat_lab[MAX_ALAT];
+extern unsigned int total_alat;
+extern Peminjaman peminjaman[MAX_PINJAMAN];
+extern unsigned int total_peminjaman;
+
+// Deklarasi fungsi
+void data();
+void simpan_data();
+int login_with_cla(const char *role, const char *username, const char *password);
+void cetak_alat();
+void admin_menu();
+void user_menu();
+int login(char *role);
+void pinjam_alat();
+void lihat_alat_pinjam();
+void kembalikan_alat();
+
+#endif
+
