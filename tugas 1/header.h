72cad8125e86b95093476161f5c591da253160d7
zahrarizkyna <zahra241@mhs.usk.ac.id>
2024-12-05 20:29:01 +0700

    Update header.h

--- tugas 1/header.h
+++ tugas 1/header.h
@@ -7,3 +7,22 @@
 
 #define MAX_ALAT 100
 #define MAX_PINJAMAN 100
+
+// Struktur untuk alat laboratorium
+typedef struct {
+    unsigned int Id_Alat;
+    char Nama_Alat[50];
+    char Merek[50];
+    char Model[50];
+    unsigned int Tahun_Produksi;
+    unsigned int Jumlah_Unit;
+    unsigned int Jumlah_Tersedia;
+} Alat_Lab;
+
+// Struktur untuk peminjaman
+typedef struct {
+    unsigned int Id_Alat;
+    char Nama_Alat[50];
+    unsigned int Jumlah_Pinjam;
+} Peminjaman;
+
