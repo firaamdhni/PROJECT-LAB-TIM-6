c4461757366b058da8e328f381286e9bbfe73040
zahrarizkyna <zahra241@mhs.usk.ac.id>
2024-12-05 21:01:18 +0700

    Update main.c

--- tugas 1/main.c
+++ tugas 1/main.c
@@ -5,3 +5,18 @@ int main(int argc, char *argv[]) {
 
     // Muat data alat dari file
     data();
+
+    // Cek login berdasarkan metode (CLA atau interaktif)
+    if (argc == 4) {
+        // Login dengan Command Line Argument
+        if (!login_with_cla(argv[1], argv[2], argv[3])) {
+            printf("Login gagal dengan Command Line Argument.\n");
+            return 1;
+        }
+        strcpy(role, argv[1]);
+    } else {
+        // Login interaktif
+        if (!login(role)) {
+            return 1;
+        }
+    }
