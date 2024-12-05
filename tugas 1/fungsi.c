f95fbb972eba40354de048f75d9d8d3a09cd1a32
Ulfakhairinaa <khairinaulfa328@gmail.com>
2024-12-05 20:34:14 +0700

    Update fungsi.c

--- tugas 1/fungsi.c
+++ tugas 1/fungsi.c
@@ -23,3 +23,37 @@ void data() {
         fclose(file);
         return;
     }
+    
+while (fscanf(file, "%u %49s %49s %49s %u %u %u",
+                  &alat_lab[total_alat].Id_Alat,
+                  alat_lab[total_alat].Nama_Alat,
+                  alat_lab[total_alat].Merek,
+                  alat_lab[total_alat].Model,
+                  &alat_lab[total_alat].Tahun_Produksi,
+                  &alat_lab[total_alat].Jumlah_Unit,
+                  &alat_lab[total_alat].Jumlah_Tersedia) != EOF) {
+        total_alat++;
+    }
+    fclose(file);
+}
+
+// Fungsi menyimpan data ke file
+void simpan_data() {
+    FILE *file = fopen("alat_lab.txt", "w");
+    if (file == NULL) {
+        printf("Gagal membuka file alat_lab.txt untuk menyimpan data.\n");
+        return;
+    }
+
+    for (unsigned int i = 0; i < total_alat; i++) {
+        fprintf(file, "%u %s %s %s %u %u %u\n",
+                alat_lab[i].Id_Alat,
+                alat_lab[i].Nama_Alat,
+                alat_lab[i].Merek,
+                alat_lab[i].Model,
+                alat_lab[i].Tahun_Produksi,
+                alat_lab[i].Jumlah_Unit,
+                alat_lab[i].Jumlah_Tersedia);
+    }
+    fclose(file);
+}
